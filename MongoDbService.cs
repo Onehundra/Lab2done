b99f0b434ea61220c7058e3cee67cd6965c2285f
Onehundra <hermanssonkevin@gmail.com>
2025-12-10 19:52:14 +0100

    lab3mongo

---
+++ MongoDbService.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2done
+{
+    public class MongoDbService
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbService(string connectionString, string databaseName)
+        {
+            var client = new MongoClient(connectionString);
+            _database = client.GetDatabase(databaseName);
+        }
+
+        public IMongoCollection<Product> Products =>
+            _database.GetCollection<Product>("Products");
+
+        public IMongoCollection<Order> Orders =>
+            _database.GetCollection<Order>("Orders");
+    }
+}
